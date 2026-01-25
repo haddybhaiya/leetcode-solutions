769bc319a0f6de2ac18523fe36eedfe0de1f666c
harshSharma <88364904+haddybhaiya@users.noreply.github.com>
2026-01-25 15:17:11 +0530

    Implement palindrome checking method in Solution class
    Add a method to check if a string is a palindrome, ignoring non-alphanumeric characters and case.

---
+++ two-pointers/valid_Palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        s = s.lower()
+        i  = 0
+        j = len(s) - 1
+        while(i < j): 
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            elif s[i] != s[j]:
+                return False
+            else:
+                i +=1
+                j -= 1
+        return True
+        
