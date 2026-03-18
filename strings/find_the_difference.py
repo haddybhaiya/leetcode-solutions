e069770244cfe4f924fbc366ebf55f9c45595501
Harsh Sharma <88364904+haddybhaiya@users.noreply.github.com>
2026-03-18 10:21:49 +0530

    Implement findTheDifference method in Solution class
    BRUTE FORCE APPROACH, optimise it later !mark

---
+++ strings/find_the_difference.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        for i in t:
+            if i in s:
+                t = t.replace(i,"",1)
+                s = s.replace(i,"",1)
+        return t
+        
