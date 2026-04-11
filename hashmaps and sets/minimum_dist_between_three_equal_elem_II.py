cd97816c9cc57017f7faae3908869664a8fc187d
Harsh Sharma <harshsharma2k614@gmail.com>
2026-04-11 20:56:24 +0530

    Add :  minimum distance for three equal elements

---
+++ hashmaps and sets/minimum_dist_between_three_equal_elem_II.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def minimumDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        seen = {}
+        mini = float('inf')
+        for i in range(len(nums)):
+            val = nums[i]
+            if val not in seen:
+                seen[val]=[i] #add mapping of val->i in seen if not there in list form
+            else:
+                seen[val].append(i) #if number is there already then append list
+                if len(seen[val]) >= 3: # if that list len is >=3
+                    m = seen[val][-3]
+                    n = seen[val][-2]
+                    o = seen[val][-1]
+
+                    dis = abs(m-n)+abs(n-o)+abs(o-m) #cal abs val
+                    mini = min(mini,dis) #cal mini
+        if mini == float('inf'):
+            return -1
+        return mini
+
