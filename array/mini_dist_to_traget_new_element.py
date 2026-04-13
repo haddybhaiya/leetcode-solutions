03443d033e0fd2a0af7db37326ff6a6db739b12a
Harsh Sharma <harshsharma2k614@gmail.com>
2026-04-13 15:30:53 +0530

    Add getMinDistance method in Solution class
    Implement getMinDistance method to find minimum distance to target.

---
+++ array/mini_dist_to_traget_new_element.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getMinDistance(self, nums, target, start):
+        """
+        :type nums: List[int]
+        :type target: int
+        :type start: int
+        :rtype: int
+        """
+        mini = float('inf')
+        for i in range(len(nums)):
+            val = nums[i]
+            if val == target:
+                mini = min(mini,abs(i-start))
+        return mini
