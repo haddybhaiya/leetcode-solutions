f4fdcd058a723951db812f15fa68b9cf334a39d8
Harsh Sharma <harshsharma2k614@gmail.com>
2026-04-28 20:25:44 +0530

    Implement getCommon method to find common value

---
+++ hashmaps and sets/minimum_common_value.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getCommon(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        sett = set(nums2)
+        for i in nums1:
+            if i in sett:
+                return i
+        return -1
+        
