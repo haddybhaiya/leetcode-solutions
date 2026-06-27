4bf7a5b1acd80b5ecece84987c63bb1aee830aa0
Harsh Sharma <harshsharma2k614@gmail.com>
2026-06-27 23:27:10 +0530

    Add solution for non-overlapping intervals problem
    Implement a solution to erase overlapping intervals by counting the number of intervals that need to be removed.

---
+++ greedy/non-overlapping-intervals.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        intervals.sort(key = lambda x:x[1])
+        end = intervals[0][1]
+        cnt = 0
+        for i in range(1,len(intervals)):
+            if intervals[i][0] < end:
+                cnt+=1
+            else:
+                end = intervals[i][1]
+        return cnt
+            
